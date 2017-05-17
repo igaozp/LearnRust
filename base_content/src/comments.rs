ec181cdf6f1e8a901ae59e4e24c4bb9c66cfa049
igaozp <igaozp@hotmail.com>
2017-05-17 20:17:08 +0800

    Add comments.rs

---
+++ base_content/src/comments.rs
@@ -0,0 +1,24 @@
+// Line comments
+let x = 5; // This is alse a line comment.
+// If you have a long explanation for something, you can put line comments next
+// to each other. Put a space between the // and your comment so that it’s
+// more readable.
+
+/// Adds one to the number given.
+///
+/// # Support Markdown
+/// ```
+/// let five = 5;
+/// assert_eq!(6, add_one(5));
+/// # fn add_one(x: i32) -> i32 {
+/// #     x + 1
+/// # }
+/// ```
+fn add_one(x: i32) -> i32 {
+    x + 1
+}
+
+//! # The Rust Standard Library
+//!
+//! The Rust Standard Library provides the essential runtime
+//! functionality for building portable Rust software.
