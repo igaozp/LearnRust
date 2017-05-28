950a1eb6a6463231d10e2baff71b37a47872c0de
igaozp <igaozp@hotmail.com>
2017-05-28 13:00:26 +0800

    Add raw_pointers.rs

---
+++ base_content/src/raw_pointers.rs
@@ -0,0 +1,20 @@
+let x = 5;
+let raw = &x as *const i32;
+
+let points_at = unsafe { *raw };
+
+println!("raw points at {}", points_at);
+
+let mut y = 10;
+let raw_mut = &mut y as *mut i32;
+
+let i: u32 = 1;
+let p_imm = *const u32 = &i as *const u32;
+
+let mut m: u32 = 2;
+let p_mut: *mut u32 = &mut m;
+
+unsafe {
+    let ref_imm: &u32 = &*p_imm;
+    let ref_mut: &mut u32 = &mut *p_mut;
+}
